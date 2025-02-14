24a35b8aae7ba76bf438e62b60a88e3cb624b990
Deep Patel <76654657+deep35@users.noreply.github.com>
2025-02-14 12:14:04 +0530

    Update iptest.py

--- iptest.py
+++ iptest.py
@@ -69,5 +69,4 @@ async def get_user_ip(request: Request):
     return {"user_ip": user_ip}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Use Render's PORT variable
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app)
